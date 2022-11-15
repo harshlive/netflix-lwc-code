9885e51b97794242748750fce770171e6d2b84f8
Harsh Ranjan <harshranjan1995@gmail.com>
2022-11-15 12:07:43 +0530

    Update netflixCloneBanner.js

--- netflixCloneBanner/netflixCloneBanner.js
+++ netflixCloneBanner/netflixCloneBanner.js
@@ -4,7 +4,7 @@ export default class NetflixCloneBanner extends LightningElement {
   @api imgUrl;
   @track headerStyle;
   @api bannerTitle;
-  @api bannerImage;
+  @api bannerImage; 
   @api bannerOverview;
   connectedCallback() {
     this.headerStyle = "background-image:url(" + this.bannerImage + ");";
